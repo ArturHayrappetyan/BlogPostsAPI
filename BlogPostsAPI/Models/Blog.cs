dfafdcb3584f5044f3d0300ccc0058bbcedbb803
Admin <Admin@MYBOOK>
2024-12-01 21:04:11 +0400

    .

--- BlogPostsAPI/Models/Blog.cs
+++ BlogPostsAPI/Models/Blog.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-
         public List<Post> Posts { get; set; } = new List<Post>();
     }
 }
